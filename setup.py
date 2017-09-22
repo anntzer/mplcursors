077cb3a105f54fdd8dc79cd8c04417f41e4d80ee
Antony Lee <anntzer.lee@gmail.com>
2017-09-22 12:01:45 -0700

    Move setupext out.

--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
-from collections import ChainMap
-import inspect
-from pathlib import Path
-
-from setuptools import find_packages, setup
-from setuptools.command.develop import develop
-from setuptools.command.install_lib import install_lib
-import versioneer
+from setupext import find_packages, setup
 
 
-# Environment variable-based activation.
-#
-# Technique inspired from http://github.com/ionelmc/python-hunter.
-#
-# Patch `develop` and `install_lib` instead of `build_py` because the latter is
-# already hooked by versioneer.
-#
 # We cannot directly import matplotlib if `MPLCURSORS` is set because
 # `sys.path` is not correctly set yet.
 #
 # The loading of `matplotlib.figure` does not go through the path entry finder
 # because it is a submodule, so we must use a metapath finder instead.
 
+@setup.register_pth_hook("mplcursors.pth")
 def _pth_hook():
     if os.environ.get("MPLCURSORS"):
         from importlib.machinery import PathFinder
@@ -61,26 +48,9 @@ def _pth_hook():
         sys.meta_path.insert(0, MplcursorsMetaPathFinder())
 
 
-class _pth_command_mixin:
-    def run(self):
-        super().run()
-        with Path(self.install_dir, "mplcursors.pth").open("w") as file:
-            file.write("import os; exec({!r}); _pth_hook()"
-                       .format(inspect.getsource(_pth_hook)))
-
-    def get_outputs(self):
-        return (super().get_outputs()
-                + [str(Path(self.install_dir, "mplcairo.pth"))])
-
-
 setup(name="mplcursors",
       description="Interactive data selection cursors for Matplotlib.",
       long_description=open("README.rst", encoding="utf-8").read(),
-      version=versioneer.get_version(),
-      cmdclass=ChainMap(
-          versioneer.get_cmdclass(),
-          {"develop": type("", (_pth_command_mixin, develop), {}),
-           "install_lib": type("", (_pth_command_mixin, install_lib), {})}),
       author="Antony Lee",
       url="https://github.com/anntzer/mplcursors",
       license="MIT",
---
+++ setupext.py
@@ -0,0 +1,87 @@
+from functools import partial
+import inspect
+import re
+from pathlib import Path
+
+import setuptools
+from setuptools import Extension, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install_lib import install_lib
+try:
+    import versioneer
+except ImportError:
+    versioneer = None
+
+
+__all__ = ["setup", "get_pybind_include",
+           "Extension", "find_packages"]
+
+
+class setup:
+    _pth_hooks = {}
+
+    def __new__(cls, **kwargs):
+
+        kwargs.setdefault("cmdclass", {})
+
+        if versioneer:
+            kwargs.setdefault("version", versioneer.get_version())
+            kwargs["cmdclass"] = dict(versioneer.get_cmdclass(),
+                                      **kwargs["cmdclass"])
+
+        class pth_hook_mixin:
+
+            def run(self):
+                super().run()
+                for fname, (name, source) in cls._pth_hooks.items():
+                    with Path(self.install_dir, fname).open("w") as file:
+                        file.write("import os; exec({!r}); {}()"
+                                   .format(source, name))
+
+            def get_outputs(self):
+                return (super().get_outputs()
+                        + [str(Path(self.install_dir, fname))
+                           for fname in cls._pth_hooks])
+
+        cmdclass = kwargs["cmdclass"]
+        cmdclass["develop"] = type(
+            "develop_with_pth_hook",
+            (pth_hook_mixin, cmdclass.get("develop", develop)),
+            {})
+        cmdclass["install_lib"] = type(
+            "install_lib_with_pth_hook",
+            (pth_hook_mixin, cmdclass.get("install_lib", install_lib)),
+            {})
+
+        setuptools.setup(**kwargs)
+
+    @classmethod
+    def register_pth_hook(cls, fname, func=None):
+        if func is None:
+            return partial(cls.register_pth_hook, fname)
+        source = inspect.getsource(func)
+        if not re.match(r"\A@setup\.register_pth_hook.*\ndef ", source):
+            raise SyntaxError("register_pth_hook must be used as a toplevel "
+                              "decorator to a function")
+        _, source = source.split("\n", 1)
+        d = {}
+        exec(source, {}, d)
+        if set(d) != {func.__name__}:
+            raise SyntaxError(
+                "register_pth_hook should define a single function")
+        cls._pth_hooks[fname] = func.__name__, source
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path.
+
+    The purpose of this class is to postpone importing pybind11 until it is
+    actually installed, so that the ``get_include()`` method can be invoked.
+    """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
